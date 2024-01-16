4eb138e022104acefc35e7b449fd7488699e6d51
RoboDoig <erskine94@gmail.com>
2024-01-16 14:23:11 -0500

    Add proto numeric interface for switching valves

--- src/Extensions/DelphiDevice.cs
+++ src/Extensions/DelphiDevice.cs
@@ -2,10 +2,12 @@
 using Bonsai.Harp;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bonsai.Expressions;
 
 namespace Extensions.Extensions
 {
