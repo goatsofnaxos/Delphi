1690bf96699c7175e66e9648f28bd33fa927c689
RoboDoig <erskine94@gmail.com>
2024-01-16 16:51:59 -0500

    Example handling of incoming message by Ui control

--- src/Extensions/DeviceVisualInterface.cs
+++ src/Extensions/DeviceVisualInterface.cs
@@ -5,11 +5,25 @@ using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.Reactive;
 
 public class DeviceVisualInterface : Combinator<HarpMessage, HarpMessage>
 {
     public override IObservable<HarpMessage> Process(IObservable<HarpMessage> source)
     {
-        return source.Do(val => {});
+        return Observable.Create<HarpMessage>(observer => {
+            
+            // Source observer is the input handler
+            var sourceObserver = Observer.Create<HarpMessage>(
+                message => {
+                    Console.WriteLine(message.ToString());
+                },
+                observer.OnError,
+                observer.OnCompleted
+            );
+
+            return new CompositeDisposable(source.SubscribeSafe(sourceObserver));
+        });
     }
 }
