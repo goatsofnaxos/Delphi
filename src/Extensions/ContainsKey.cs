0bb0eee1c1040c8cea59e312a9748f37cf068f13
RoboDoig <erskine94@gmail.com>
2024-01-18 16:59:23 -0500

    Add dictionary helper operator functions

---
+++ src/Extensions/ContainsKey.cs
@@ -0,0 +1,19 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class ContainsKey
+{
+    public string Key {get; set;}
+
+    public IObservable<bool> Process(IObservable<IDictionary<string, string>> source)
+    {
+        return source.Select(value => value.ContainsKey(Key));
+    }
+}
