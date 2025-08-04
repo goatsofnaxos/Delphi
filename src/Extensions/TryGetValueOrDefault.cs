0d09eb1ae7b0c37a65be04b99f5364407e1e2887
RoboDoig <erskine94@gmail.com>
2025-08-04 13:52:35 +0100

    Implement random rule selection

--- src/Extensions/TryGetValueOrDefault.cs
+++ src/Extensions/TryGetValueOrDefault.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using RuleSchema;
 
 [Combinator]
 [Description("")]
@@ -13,13 +14,15 @@ public class TryGetValueOrDefault
     public string Key {get; set;}
     public string DefaultString {get; set;}
 
-    public IObservable<string> Process(IObservable<IDictionary<string, string>> source)
+    public IObservable<List<StateProbability>> Process(IObservable<IDictionary<string, List<StateProbability>>> source)
     {
         return source.Select(value => {
-            string element;
+            List<StateProbability> element;
             bool contained = value.TryGetValue(Key, out element);
             
-            return contained ? element : DefaultString;
+            return contained ? element : new List<StateProbability> {
+                new StateProbability { Name = DefaultString, Probability = 1}
+            };
         });
     }
 }
---
+++ src/Extensions/Utils.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RuleSchema;
+
+public static class Utils {
+    static Random rnd = new Random(DateTime.Now.Millisecond);
+
+    public static string GetRandomState(List<StateProbability> stateProbabilities) {
+
+        var totalProbability = stateProbabilities.Sum(x => 1.0 / x.Probability);
+        if (totalProbability < 0.9999999999999 || totalProbability > 1.0000000000001) {
+            throw new ArgumentException("Total probability must sum to 1");
+        }
+
+        var randomVal = rnd.NextDouble();
+        foreach (StateProbability stateProbability in stateProbabilities) {
+            var probability = 1.0/stateProbability.Probability;
+            if (randomVal < probability) {
+                return stateProbability.Name;
+            }
+            randomVal -= probability;
+        }
+
+        return null;
+    }
+}
