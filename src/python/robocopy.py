c041fdd48b4c638754754ab99dfe49d3659e7a38
fink-lab-neuro <finklabneurobio@gmail.com>
2025-09-19 12:26:23 -0500

    Update robocopy.py

--- src/python/robocopy.py
+++ src/python/robocopy.py
@@ -7,12 +7,12 @@ from pathlib import Path
 ############################################################
 # Comment when running an experiment; uncomment when testing
 #
-parser = argparse.ArgumentParser(description="Moves data files from the specified source folder to a remote storage folder.")
-parser.add_argument('source', type=str, help="The path where the local data is stored.")
-parser.add_argument('destination', type=str, help="The remote path where data is to be stored.")
-args = parser.parse_args()
-source = Path(args.source)
-destination = Path(args.destination)
+#parser = argparse.ArgumentParser(description="Moves data files from the specified source folder to a remote storage folder.")
+#parser.add_argument('source', type=str, help="The path where the local data is stored.")
+#parser.add_argument('destination', type=str, help="The remote path where data is to be stored.")
+#args = parser.parse_args()
+#source = Path(args.source)
+#destination = Path(args.destination)
 #
 # Comment when running an experiment; uncomment when testing
 ############################################################
@@ -20,8 +20,8 @@ destination = Path(args.destination)
 ############################################################
 # Comment when testing; uncomment when running an experiment
 #
-#source = Path("C:/Users/bvw8415/Delphi/HIW3/")
-#destination = Path("C:/Users/bvw8415/OneDrive - Northwestern University/Andrew Jacob Pixley Fink - FinkLab-SharedDrive/DATA/Behavior/Delphi/RobocopyHIW/")
+source = Path("C:/Users/bvw8415/Delphi/HIW3/")
+destination = Path("C:/Users/bvw8415/OneDrive - Northwestern University/Andrew Jacob Pixley Fink - FinkLab-SharedDrive/DATA/Behavior/Delphi/RobocopyHIW/")
 #
 # Comment when testing; uncomment when running an experiment
 ############################################################
