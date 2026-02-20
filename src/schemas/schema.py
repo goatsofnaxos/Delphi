57b66d4c34c9ece277e1c2aebb4c49d5e649a8ca
Prattbuw <prattbuw@gmail.com>
2026-02-19 16:28:25 -0800

    Aquire from multiple cameras

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -60,6 +60,16 @@ class CameraSettings1(BaseModel):
     ffmpeg_input: str
     ffmpeg_output: str
 
+class CameraSettings1(BaseModel):
+    camera_name: str
+    frame_rate: int = Field(ge=0)
+    exposure: int = Field(ge=0)
+    duty_cycle: float = Field(ge=0, le=1)
+    serial_number: str
+    ffmpeg_input: str
+    ffmpeg_output: str
+
+
 class HardwareSchema(BaseModel):
     subject_id: str
     session_time: str
@@ -68,10 +78,8 @@ class HardwareSchema(BaseModel):
     robocopy_script_path: str
     delphi_controller: DelphiController
     camera_settings: CameraSettings
-    camera_settings1: CameraSettings1
-    fluidic_settings: FluidicSettings 
-    delux_driver: DeLuxDriver
-    white_rabbit: WhiteRabbit
+    # camera_settings1: CameraSettings1
+
 
 class StateDefinition(BaseModel):
     name: str
