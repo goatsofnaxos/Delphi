3ad3c0f4ebc6bd5b3895ffb2ab3adfdf45e611d6
Prattbuw <prattbuw@gmail.com>
2026-05-21 15:36:42 -0700

    Consolidate dataset

--- src/delphi-data/scripts/build_dataset.py
+++ src/delphi-data/scripts/build_dataset.py
@@ -2,7 +2,5 @@ from pathlib import Path
 
 from delphi_data.curation import consolidate_session_runs
 
-session_dir = Path(
-    r"C:\Users\brandon.pratt\Desktop\data\test_data\842456\2026-03-20T20-23-05"
-)
+session_dir = Path(r"\\allen\aind\stage\chronic\data\842456\2026-03-20T20-23-05")
 consolidate_session_runs(session_dir)
