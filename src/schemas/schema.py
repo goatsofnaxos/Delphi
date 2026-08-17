aeb3a647b8e0c449457ea28de9a887d806a61d6d
Prattbuw <prattbuw@gmail.com>
2026-08-17 14:12:42 -0700

    Refactor delphi schema

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -6,17 +6,41 @@ import os
 
 
 class DelphiController(BaseModel):
-    final_valve_energized_time_us: int = Field(ge=0)
+    com_port: str
+    poke_pin: int
     max_odor_delivery_time_us: int = Field(ge=0)
     min_poke_time_us: int = Field(ge=0)
     min_odor_delivery_time_us: int = Field(ge=0)
-    odor_transition_time_us: int = Field(ge=0)
-    poke_pin: int = Field(default=22, ge=0)
-    vacuum_setup_time_us: int = Field(ge=0)
-    vacuum_close_time_us: int = Field(ge=0)
+    odor_setup_time_us: int = Field(ge=0)
     odor_dwell_time_us: int = Field(ge=0)
-    com_port: str
 
+class FluidicSettings(BaseModel):
+    leak_flowmeter_adc: int = Field(ge=0)
+    leak_threshold: float = Field(ge=0)
+    manual_flometer_adc: int = Field(ge=0)
+    manual_flowmeter_target_flow_rate: float = Field(ge=0)
+    manual_flowmeter_flow_rate_tolerance: float = Field(ge=0)
+    proportional_valve_flowmeter_0_adc: int = Field(ge=0)
+    proportional_valve_flowmeter_0_target_flowrate: float = Field(ge=0)
+    proportional_valve_flowmeter_1_adc: int = Field(ge=0)
+    proportional_valve_flowmeter_1_target_flowrate: float = Field(ge=0)
+    proportional_valve_flowmeter_2_adc: int = Field(ge=0)
+    proportional_valve_flowmeter_2_target_flowrate: float = Field(ge=0)
+
+class DeLuxDriver(BaseModel):
+    com_port: str
+    ch1_current: int = Field(ge=0)
+    ch1_pulse_frequency: int = Field(ge=0)
+    ch1_pulse_duty_cycle: float = Field(ge=0)
+    ch2_current: int = Field(ge=0)
+    ch2_pulse_frequency: int = Field(ge=0)
+    ch2_pulse_duty_cycle: float = Field(ge=0)
+    ch3_current: int = Field(ge=0)
+    ch3_pulse_frequency: int = Field(ge=0)
+    ch3_pulse_duty_cycle: float = Field(ge=0)
+
+class WhiteRabbit(BaseModel):
+    com_port: str
 
 class CameraSettings(BaseModel):
     camera_name: str
@@ -27,7 +51,6 @@ class CameraSettings(BaseModel):
     ffmpeg_input: str
     ffmpeg_output: str
 
-
 class CameraSettings1(BaseModel):
     camera_name: str
     frame_rate: int = Field(ge=0)
@@ -37,20 +60,22 @@ class CameraSettings1(BaseModel):
     ffmpeg_input: str
     ffmpeg_output: str
 
-
 class HardwareSchema(BaseModel):
+    subject_id: str
+    session_time: str
     logging_root_path: str
     remote_transfer_root_path: str
     delphi_controller: DelphiController
     camera_settings: CameraSettings
     camera_settings1: CameraSettings1
-
+    fluidic_settings: FluidicSettings 
+    delux_driver: DeLuxDriver
+    white_rabbit: WhiteRabbit
 
 class StateDefinition(BaseModel):
     name: str
     odor_index: int
     transitions_to: List[str]
-
 
 class DelphiRule(BaseModel):
     rule_name: str
@@ -59,10 +84,8 @@ class DelphiRule(BaseModel):
     init_odor_index: int
     state_definitions: List[StateDefinition]
 
-
 class RuleSchema(BaseModel):
     rule: DelphiRule
-
 
 if __name__ == "__main__":
     hardware_schema = HardwareSchema.model_json_schema()
